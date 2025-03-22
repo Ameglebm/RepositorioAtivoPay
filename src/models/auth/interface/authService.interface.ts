40de09b7fdf7355106224191b9a39511f9c685e7
Ameglebm <amegleeu@gmail.com>
2025-03-22 19:23:56 -0300

    atualizado

--- src/models/auth/interface/authService.interface.ts
+++ src/models/auth/interface/authService.interface.ts
@@ -1,6 +1,5 @@
 export interface AuthService {
-    register(email: string, password: string): Promise<void>;
-    login(email: string, password: string): Promise<string>;
+    register(email: string, senha: string): Promise<void>;
+    login(email: string, senha: string): Promise<string>;
     logout(token: string): Promise<void>;
-    refreshToken(token: string): Promise<string>;
 }
